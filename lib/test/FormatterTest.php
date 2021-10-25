ee0100b24103e6e1fcdb20987fac528ee68b8710
Kai Kühn <kuehn@diqa-pm.com>
2021-10-25 20:46:18 +0200

    added first test

--- lib/test/FormatterTest.php
+++ lib/test/FormatterTest.php
@@ -1,17 +1,45 @@
 <?php
+namespace DIQA\Formatter;
 
 use PHPUnit\Framework\TestCase;
 
 final class FormatterTest extends TestCase
 {
-    public function test(): void
+    public function testWithBorder(): void
     {
-        //TODO: add tests
-        /*$this->assertEquals(
-            'user@example.com',
-            'user@example.com'
-        );*/
+        $config = new Config(
+            [20, 30, 30],
+            [Config::LEFT_ALIGN, Config::LEFT_ALIGN, Config::RIGHT_ALIGN],
+            ['padding' => false, 'border' => true]
+        );
+
+        $formatter = new Formatter($config);
+
+        $formattedOutput = $formatter->format([
+            ["row 1 column 1", "row 1 column 2", "254,00"],
+            ["row 2 column 1", "row 2 column 2", "1.233,00"],
+            ["row 3 column 1", "row 3 column 2 ", "424,21"],
+        ]);
+
+        print "\n$formattedOutput";
+
+        $expectedOutput = <<<EOT
+┌──────────────────┬─────────────────────────────┬─────────────────────────────┐
+│row 1 column 1    │row 1 column 2               │                       254,00│
+├──────────────────┼─────────────────────────────┼─────────────────────────────┤
+│row 2 column 1    │row 2 column 2               │                     1.233,00│
+├──────────────────┼─────────────────────────────┼─────────────────────────────┤
+│row 3 column 1    │row 3 column 2               │                       424,21│
+└──────────────────┴─────────────────────────────┴─────────────────────────────┘
+EOT;
+        $this->assertEquals(
+            self::normalize($expectedOutput),
+            self::normalize($formattedOutput)
+        );
     }
 
+    static function normalize($output) {
+        return str_replace("\r", "", $output);
+    }
 
 }
